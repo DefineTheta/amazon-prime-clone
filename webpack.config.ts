0249cc9c38c39eb1360c4cae8d3a4b85524ba296
DefineTheta <partheyb@gmail.com>
2021-02-09 07:02:25 -0500

    First commit

---
+++ webpack.config.ts
@@ -0,0 +1,55 @@
+import path from 'path';
+import webpack, { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+
+const webpackConfig = (env): Configuration => ({
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+		plugins: [new TsconfigPathsPlugin()],
+	},
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'build.js',
+		assetModuleFilename: 'images/[hash][ext][query]',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				options: {
+					transpileOnly: true,
+				},
+				exclude: /dist/,
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new webpack.DefinePlugin({
+			'process.env.PRODUCTION': env.production || !env.development,
+			'process.env.NAME': JSON.stringify(require('./package.json').name),
+			'process.env.VERSION': JSON.stringify(require('./package.json').version),
+		}),
+		new ForkTsCheckerWebpackPlugin({
+			eslint: {
+				files: './src/**/*.{ts,tsx,js,jsx}', // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+			},
+		}),
+	],
+});
+
+export default webpackConfig;
